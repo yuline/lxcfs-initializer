3c681e30d573be85a5b5dd49ee08113d03e72e41
yul <yul@inke.cn>
2019-03-06 11:38:57 +0800

    modify  start.sh
    Signed-off-by: yul <yul@inke.cn>

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -6,7 +6,7 @@ nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
         sed -i "/^lxcfs \/var\/lib\/lxcfs fuse.lxcfs/d" /etc/mtab
 
 # Prepare
-mkdir -p /usr/local/lib/lxcfs /var/lib/lxcfs
+mkdir -p /usr/local/lib/lxcfs /var/lib/lxcfs || true
 rm -rf /var/lib/lxcfs/*
 
 # Mount
