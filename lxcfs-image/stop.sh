91233ac234ac043757e363126aeb4c1722aac2d6
yul <yul@inke.cn>
2019-03-06 11:28:47 +0800

    add stop.sh
    Signed-off-by: yul <yul@inke.cn>

---
+++ lxcfs-image/stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Cleanup
+nsenter -m/proc/1/ns/mnt fusermount -u /var/lib/lxcfs 2> /dev/null || true
+nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
+                    sed -i "/^lxcfs \/var\/lib\/lxcfs fuse.lxcfs/d" /etc/mtab
+
+rm -rf /var/lib/lxcfs/*
